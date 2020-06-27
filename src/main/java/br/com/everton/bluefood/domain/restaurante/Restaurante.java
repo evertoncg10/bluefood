33055f11226e8e56fb28f27994eb19af42cde587
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2020-06-27 11:32:32 -0300

    Buscando restaurantes - Parte 2.

--- src/main/java/br/com/everton/bluefood/domain/restaurante/Restaurante.java
+++ src/main/java/br/com/everton/bluefood/domain/restaurante/Restaurante.java
@@ -2,10 +2,12 @@ package br.com.everton.bluefood.domain.restaurante;
 
 import java.math.BigDecimal;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
@@ -23,6 +25,7 @@ import org.springframework.web.multipart.MultipartFile;
 import br.com.everton.bluefood.domain.usuario.Usuario;
 import br.com.everton.bluefood.infrastructure.web.validator.UploadConstraint;
 import br.com.everton.bluefood.util.FileType;
+import br.com.everton.bluefood.util.StringUtils;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -60,7 +63,7 @@ public class Restaurante extends Usuario {
     @Max(120)
     private Integer tempoEntregaBase;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "restaurante_has_categoria", joinColumns = @JoinColumn(name = "restaurante_id"), inverseJoinColumns = @JoinColumn(name = "categoria_restaurante_id"))
     @Size(min = 1, message = "O restaurante precisa ter pelo menos uma categoria")
     @ToString.Exclude
@@ -75,6 +78,15 @@ public class Restaurante extends Usuario {
         }
 
         this.logotipo = String.format("%04d-logo.%s", getId(), FileType.of(logotipoFile.getContentType()).getExtension());
+    }
+
+    public String getCategoriasAsText() {
+        Set<String> strings = new LinkedHashSet<>();
+        for (CategoriaRestaurante categoria : categorias) {
+            strings.add(categoria.getNome());
+        }
+
+        return StringUtils.concatenate(strings);
     }
 
 }
